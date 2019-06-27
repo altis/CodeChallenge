a3709bd4bfa8da9bf21b0f98554aee58f4442538
CN112 <cn112@CN112.local>
2019-06-27 16:19:58 +0100

    Task to detetct self and cyclic dependancy completed

--- CodingChallenge.java
+++ CodingChallenge.java
@@ -19,11 +19,7 @@ class CodingChallenge {
         do{
             job = scanner. nextLine();
             if(!new String("submit").equals(job)) {
-
-                System.out.println(job.charAt(0));
-
                 if(job.length()==6){
-                    System.out.println(job.charAt(5));
                     jobs_list.add(new JobClass(job.charAt(0),job.charAt(5)));
                 }else jobs_list.add(new JobClass(job.charAt(0),' '));
             }
@@ -48,26 +44,48 @@ class CodingChallenge {
         while(pointer < jobs_list.size()){
             for(int handle = pointer; handle < jobs_list.size(); handle++){
                 if(jobs_list.get(pointer).dependancy == jobs_list.get(handle).job ){
-                    System.out.println("Dependancy");
                     System.out.println(handle);
                     JobClass jobToAdvance = jobs_list.remove(handle);
                     jobs_list.add(pointer, new JobClass(jobToAdvance.job,jobToAdvance.dependancy));
                     pointer--;
                     break;
                 }
-
             }
             pointer++;
         }
     }
+    public boolean checkCyclicDependancy(){
+        int pointer = 0; char start; char visit;
+        while(pointer < jobs_list.size()){
+            //Code to check dependant on itself
+            start = jobs_list.get(pointer).job;
+            visit = jobs_list.get(pointer).dependancy;
+            if(start == visit) return true; // If not check for any circular depandancy
+            else {
+                for (int handle = 0; handle < jobs_list.size(); handle++) {
+                    if (pointer != handle) {
+                        if (jobs_list.get(handle).job == visit) {
+                            visit = jobs_list.get(handle).dependancy;
+                        }
+                    }
+                }
+                if(start == visit) return  true;
+            }
+            pointer ++;
+        }
+        return false;
+    }
     public static void main(String args[]){
-
         CodingChallenge codeCh = new CodingChallenge();
-
-        System.out.println("Hello");
         codeCh.inputJobs();
-        codeCh.scheduleJobs();
-        codeCh.printJobs();
+
+        if(codeCh.checkCyclicDependancy()){
+            System.out.println("Detected Depandancy ! Jobs Terminated !");
+
+        } else{
+            codeCh.scheduleJobs();
+            codeCh.printJobs();
+        }
 
     }
 }
